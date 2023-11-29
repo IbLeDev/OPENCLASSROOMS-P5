1cbd6b07700c4817df3b037e55f09df10fe82916
IbLeDev <ibrahim8.fofana@gmail.com>
2023-11-29 15:34:34 +0100

    Scroll infini terminé

--- assets/script.js
+++ assets/script.js
@@ -20,12 +20,12 @@ const slides = [
 
 const banner_img = document.querySelector("#banner img");
 const banner_p = document.querySelector("#banner p");
-const arrow_left = document.getElementsByClassName("arrow_left");
-const arrow_right = document.getElementsByClassName("arrow_right");
+const arrow_left = document.getElementsByClassName("arrow_left")[0];
+const arrow_right = document.getElementsByClassName("arrow_right")[0];
 const dots = document.getElementsByClassName("dot");
 
 let activeSlide = 0;
-let selectedDot = document.getElementsByClassName("dot_selected");
+let selectedDot = document.getElementsByClassName("dot_selected")[0];
 
 function lastImage() {
   if (activeSlide == 3) {
@@ -37,22 +37,30 @@ function lastImage() {
 
 function slideLeft() {
   if (activeSlide > 0) {
-    selectedDot[0].classList.remove("dot_selected");
+    selectedDot.classList.remove("dot_selected");
     activeSlide--;
     dots[activeSlide].classList.add("dot_selected");
     banner_img.setAttribute(
       "src",
-      "./assets/images/slideshow/slide" + (activeSlide + 1) + ".jpg"
+      "./assets/images/slideshow/slide" + (activeSlide + 1) + lastImage()
     );
     banner_p.innerHTML = slides[activeSlide].tagLine;
-    console.log("clic gauche");
+    selectedDot = document.getElementsByClassName("dot_selected")[0];
   } else {
-	activeSlide = 3;
+    selectedDot.classList.remove("dot_selected");
+    activeSlide = 3;
+    dots[activeSlide].classList.add("dot_selected");
+    selectedDot = document.getElementsByClassName("dot_selected")[0];
+    banner_img.setAttribute(
+      "src",
+      "./assets/images/slideshow/slide" + (activeSlide + 1) + lastImage()
+    );
+    banner_p.innerHTML = slides[activeSlide].tagLine;
   }
 }
 function slideRight() {
   if (activeSlide < 3) {
-    selectedDot[0].classList.remove("dot_selected");
+    selectedDot.classList.remove("dot_selected");
     activeSlide++;
     dots[activeSlide].classList.add("dot_selected");
     banner_img.setAttribute(
@@ -60,13 +68,19 @@ function slideRight() {
       "./assets/images/slideshow/slide" + (activeSlide + 1) + lastImage()
     );
     banner_p.innerHTML = slides[activeSlide].tagLine;
+    selectedDot = document.getElementsByClassName("dot_selected")[0];
   } else {
-	activeSlide = 0;
-	
+    selectedDot.classList.remove("dot_selected");
+    activeSlide = 0;
+    dots[activeSlide].classList.add("dot_selected");
+    selectedDot = document.getElementsByClassName("dot_selected")[0];
+    banner_img.setAttribute(
+      "src",
+      "./assets/images/slideshow/slide" + (activeSlide + 1) + lastImage()
+    );
+    banner_p.innerHTML = slides[activeSlide].tagLine;
   }
 }
 
-console.log(arrow_left);
-
-arrow_left[0].addEventListener("click", slideLeft());
-arrow_right[0].addEventListener("click", slideRight());
+arrow_left.addEventListener("click", slideLeft);
+arrow_right.addEventListener("click", slideRight);
